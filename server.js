b649b589842406e6ea4e8411a16358b6b3feef57
Kim McCallum <kchaase@gmail.com>
2020-03-24 15:58:26 -0600

    ready to deploy to Heroku

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const MOVIES = require('./movies-data-small.json');
 const app = express();
 
 // Setup the middleware
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 
@@ -16,7 +17,6 @@ app.use(cors());
 app.use(function validateBearerToken(req, res, next){
     const authToken = req.get('Authorization');
     const apiToken = process.env.API_TOKEN;
-    console.log('validate bearer token middleware')
 
     if (!authToken || authToken.split(' ')[1] !== apiToken){
         return res.status(401).json({error: "Unauthorized request"})
@@ -46,9 +46,18 @@ app.get('/movie', function handleGetMovies(req, res){
     res.json(response)
 })
 
-const PORT = 8000;
+app.use((error, req, res, next) => {
+    let response
+    if(process.env.NODE_ENV === 'production'){
+        response = { error: { message: 'server error'}}
+    } else {
+        response = { error }
+    }
+    res.status(500).json(response)
+})
+
+const PORT =  process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-    console.log(`Movies server listening at http://localhost:${PORT}`);
 })
 
